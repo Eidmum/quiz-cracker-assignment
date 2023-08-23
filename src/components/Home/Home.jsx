f8d31409de5f77ba64f95b541aee4ccaaaf590a6
eidmum <mdzuneadabedineidmum@gmail.com>
2023-08-24 00:18:40 +0600

    header created

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="card text-bg-dark">
         <img src="quiz.jpg" className="card-img" alt="..." />
         <div className="card-img-overlay d-flex flex-column justify-content-center align-items-center">
-          <h5 className="card-title">Moodle</h5>
+          <h5 className="card-title text-primary">Moodle</h5>
           <p className="card-text">
             Moodle is a widely used open-source learning management system (LMS)
             or e-learning platform.
