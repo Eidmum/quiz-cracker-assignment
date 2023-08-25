f53f85e1f11a71582af73985057a1b1626e79761
eidmum <mdzuneadabedineidmum@gmail.com>
2023-08-26 04:06:41 +0600

    edit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,17 +8,12 @@ const Home = () => {
   return (
     <div className="header">
       <div className="card text-bg-dark">
-        <img src="quiz.jpg" className="card-img" alt="..." />
+        <img src="quiz.jpg" className="card-img img-fluid" alt="..." />
         <div className="card-img-overlay d-flex flex-column justify-content-center align-items-center">
-          <h5 className="card-title text-primary">Moodle</h5>
+          {/* <h5 className="card-title text-primary">Moodle</h5> */}
           <p className="card-text">
             Moodle is a widely used open-source learning management system (LMS)
             or e-learning platform.
-          </p>
-          <p className="card-text">
-            <button type="button" className="btn btn-secondary">
-              Explore
-            </button>
           </p>
         </div>
       </div>
