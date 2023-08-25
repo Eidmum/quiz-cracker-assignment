1430364f099841a397b64d8e394d3d15bbe1d569
eidmum <mdzuneadabedineidmum@gmail.com>
2023-08-26 01:04:20 +0600

    context router for wrong or correct answer created

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Option.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { AnsContainer } from "../Quiz/Quiz";
+
 const Option = ({ option, index, correctAnswer }) => {
+  const { correctCounter, wrongCounter } = useContext(AnsContainer);
+
   const [css, setCss] = useState({});
   const correctStyle = {
     backgroundColor: "green",
@@ -19,11 +23,14 @@ const Option = ({ option, index, correctAnswer }) => {
     if (selectedAnswer === correctAnswer) {
       setCss(correctStyle);
       toast("Answer is correct");
+      correctCounter(selectedAnswer);
     } else {
       setCss(wrongStyle);
       toast("Answer is wrong");
+      wrongCounter(selectedAnswer);
     }
   };
+
   return (
     <div
       className="option col-md-5 d-flex justify-content-center align-items-center"
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import IndividualQuestion from "../IndividualQuestion/IndividualQuestion";
 import "./Quiz.css";
+import { createContext } from "react";
+export const AnsContainer = createContext({});
 const Quiz = () => {
   const questionsData = useLoaderData();
   const { id, name, logo, questions } = questionsData;
+  const [wrong, setWrong] = useState([]);
+  const wrongCounter = (wrongQuestion) => {
+    let wrongArray = [...wrong, wrongQuestion];
+    setWrong(wrongArray);
+  };
+
+  const [correct, setCorrect] = useState([]);
+  const correctCounter = (correctQuestion) => {
+    let correctArray = [...correct, correctQuestion];
+    setCorrect(correctArray);
+  };
+  console.log(wrong, correct);
 
   return (
     <div>
       <div className="quiz">
         <h1 className="text-center py-5">{name} Questions</h1>
-        {questions.map((singleQuestion, index) => (
-          <IndividualQuestion
-            quesNo={index + 1}
-            singleQuestion={singleQuestion}
-            key={singleQuestion.id}
-          ></IndividualQuestion>
-        ))}
+        <AnsContainer.Provider value={{ correctCounter, wrongCounter }}>
+          {questions.map((singleQuestion, index) => (
+            <IndividualQuestion
+              quesNo={index + 1}
+              singleQuestion={singleQuestion}
+              key={singleQuestion.id}
+            ></IndividualQuestion>
+          ))}
+        </AnsContainer.Provider>
       </div>
       <div>This is correct panel</div>
     </div>
